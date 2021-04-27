fb43a68e378f536411b10bf415399e6f3089330a
Thyrlky <tahayerlikayaa61@gmail.com>
2021-04-27 16:04:29 +0300

    İlk Commit
    İlk commit / temayı projeye dahil etme

---
+++ routes/web.php
@@ -0,0 +1,33 @@
+<?php
+
+use App\Http\Controllers\PagesController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', [PagesController::class, 'index']);
+
+
+// Demo routes
+Route::get('/datatables', [PagesController::class, 'datatables']);
+Route::get('/ktdatatables', [PagesController::class, 'ktDatatables']);
+Route::get('/select2', [PagesController::class, 'select2']);
+Route::get('/jquerymask', [PagesController::class, 'jQueryMask']);
+Route::get('/icons/custom-icons', [PagesController::class, 'customIcons']);
+Route::get('/icons/flaticon', [PagesController::class, 'flaticon']);
+Route::get('/icons/fontawesome', [PagesController::class, 'fontawesome']);
+Route::get('/icons/lineawesome', [PagesController::class, 'lineawesome']);
+Route::get('/icons/socicons', [PagesController::class, 'socicons']);
+Route::get('/icons/svg', [PagesController::class, 'svg']);
+
+// Quick search dummy route to display html elements in search dropdown (header search)
+Route::get('/quick-search', [PagesController::class, 'quickSearch'])->name('quick-search');
